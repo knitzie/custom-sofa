bb3a6069c7c4ca691265f6666c308e5a7e171be7
knitzie <anjadehn86@gmail.com>
2014-08-28 15:31:46 +0200

    build in angular broadcast and angular snap

---
+++ app/scripts/service.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Directives */
+
+angular.module('customSofaApp')
+    .service('sharedProperties', function ($rootScope) {
+
+        return {
+            setSummary: function(summary) {
+                /* Sets the rootScope variable, which all controllers can handle
+                 * Inside the controller, call $scope.$on('SummaryChanged', function(){}) */
+                $rootScope.$broadcast('SummaryChanged', summary);
+            }
+        };
+    })
+    ;
